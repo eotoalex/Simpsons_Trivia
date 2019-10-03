7c7c706d15f1fb92636774eb3c5dfe9b3ecf7d0a
Alex Puello <apuello88@yahoo.com>
2019-10-02 21:40:52 -0400

    Fixed the buttons but still need to debug the timer while the user is deciding, and matching correct answers with incorrect user choices.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,24 @@
+// Variables
+
 var counter = 0;
 var timer = 60;
 var correct = 0;
 var incorrect = 0;
 var lives = 5;
+
+// Arrays and of questions.
+
 questionsArr = ["In the Simpsons, what is this character's name?",
                 "?2","?3","?4","?5","?6","?7","?8","?9","?10"];
+
+// Arrays of answers.
 answers = [
     {choice1:"Hobbs Moman",
     choice2: "Billy Crystal",
     choice3: "Moe Sislack",
     choice4: "Abe Simpson",
-    answer:"Hobbs Moman"}
+    correctAns:"1"
+    }
 ];
 
 // This sets up the decrementing of time, displaying of questions,incrementing of the counter,displaying of the timer and the resetting of the timer as well.
@@ -22,10 +30,10 @@ function minuteDecrement () {
     $("#display-question").html(questionsArr[counter]);
     displayChoices ()
     
+    
     if(timer === 0){
         counter++;
-        
-        $("#display-question").html(questionsArr[counter]);
+        lives--;
         timer=60;
     }
 }
@@ -35,15 +43,35 @@ function minuteDecrement () {
 
 function displayChoices (){
     
-    $(".choices").html("<button >"+answers[counter].choice1+"</button>")
-    $(".choices").append("<button >" + answers[counter].choice2 + "</button>")
-    $(".choices").append("<button >" + answers[counter].choice3 + "</button>")
-    $(".choices").append("<button >" + answers[counter].choice4 + "</button>")
+    $(".choice1").html("<button>" + answers[counter].choice1 + "</button>")
+    $(".choice2").html("<button>" + answers[counter].choice2 + "</button>")
+    $(".choice3").html("<button>" + answers[counter].choice3 + "</button>")
+    $(".choice4").html("<button>" + answers[counter].choice4 + "</button>")
 
-    $(".choices").on("click", function (event){
-        var pressedKey = $(this).val();
+    $("button").on("click", function (event){
+        var clickedButton = $(this).val();
+        var turns = 2;
+        
 
-        alert(pressedKey)
+        // console.log(typeof clickedButton,typeof answers[0].correctAns );
+
+        
+
+        if (clickedButton === answers[counter].correctAns){
+            counter++;
+            correct++;
+            timer=60;
+            console.log("You win.");
+
+
+        }else if (clickedButton !== answers[counter].correctAns && turns !== 0){ 
+            incorrect++;
+            turns--;
+            console.log("You lose.")
+        }else{}
+
+        
+        
     });
 
 }
